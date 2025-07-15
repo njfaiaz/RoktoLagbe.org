0dce59251dd94721987e811f2a1c5ee26928bfcc
Faiaz <njfaiaz@gmail.com>
2025-07-15 23:40:24 +0600

    Route Add

--- config/backup.php
+++ config/backup.php
@@ -151,7 +151,8 @@ return [
              * The disk names on which the backups will be stored.
              */
             'disks' => [
-                'google',
+                'local'
+                // 'google',
 
             ],
         ],
